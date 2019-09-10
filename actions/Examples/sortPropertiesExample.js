e3cf4c9bd427dc31760cd72c0eac2b5e480416bb
Craig Sorensen <craigs@uoregon.edu>
2019-09-10 16:49:31 -0700

    Example: How to sort property objects for use with vRA dropdown.

---
+++ actions/Examples/sortPropertiesExample.js
@@ -0,0 +1,32 @@
+/* 	Example: vRO Sorting Properties Workaround
+		While it doesn't appear you can sort a properties object itself directly in vRO, you can still achieve the same results using this workaround. 
+		(Big thanks to Shazad for this tip!)
+
+		TLDR; create an array of property objects and they will be returned in the order listed in the array.
+
+		Author: Craig Sorensen
+*/
+
+
+function addToArray(prop){  
+     System.log("Adding: " + prop.get("value"));  
+     array.push(prop);  
+}  
+  
+var array = [];  
+  
+for(var i = 0 ; i < 2 ; i++ ) {  
+     var property = new Properties();  
+  
+     property.put('label','label'+i);  
+     property.put('value','value'+i);  
+  
+     addToArray(property);  
+}  
+  
+for each (item in array){  
+     System.log("item: " + item.get("label"));  
+     System.log("item: " + item.get("value"));  
+}  
+  
+return array;  
